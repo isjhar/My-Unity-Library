952a30b3553fb0d806b221dd2dc15ef699e8318d
Isjhar <isjhar@gmail.com>
2016-03-17 11:43:21 +0700

    nambah contoh penggunaan screen exchange dan drag and drop

---
+++ Assets/Scripts/FreeDropZone.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class FreeDropZone : DropZone
+{
+	#region implemented abstract members of DropZone
+
+	public override void OnDrop (PointerEventData eventData)
+	{
+		Debug.Log("OnDrop Free Drop Zone");
+		if(eventData.pointerDrag)
+		{
+			DragHandler draggedItem = eventData.pointerDrag.GetComponent<DragHandler>();
+			if(draggedItem != null && draggedItem.enabled)
+				draggedItem.transform.SetParent(transform, true);
+		}
+	}
+	#endregion
+
+
+}
---
+++ Assets/Scripts/SlotDropZone.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class SlotDropZone : DropZone 
+{
+	#region implemented abstract members of DropZone
+
+	public override void OnDrop (PointerEventData eventData)
+	{
+		Debug.LogFormat("OnDrop Slot Drop Zone {0} : {1}", eventData.pointerDrag.name, eventData.pointerDrag.tag);
+		if(eventData.pointerDrag)
+		{
+			// spawp image
+			DragHandler droppedItemDragHandler = eventData.pointerDrag.GetComponent<DragHandler>();
+
+			if(droppedItemDragHandler != null && droppedItemDragHandler.enabled)
+			{
+				// if current item exist in this drop zone
+				DragHandler myCurrentItem = transform.gameObject.GetComponentInChildren<DragHandler>();
+				if(myCurrentItem != null)
+				{
+					myCurrentItem.transform.SetParent(droppedItemDragHandler.MyOldParent, false);
+					ResetRectTransform(myCurrentItem.gameObject);	
+				}
+					
+				droppedItemDragHandler.MyOldParent = transform;
+				droppedItemDragHandler.transform.SetParent(transform, false);
+				ResetRectTransform(eventData.pointerDrag);
+			}
+		}
+	}
+
+	#endregion
+
+	private void ResetRectTransform(GameObject item)
+	{
+		RectTransform currentItemRect = item.GetComponent<RectTransform>();
+		currentItemRect.anchorMin = new Vector2(0.5f, 0.5f);
+		currentItemRect.anchorMax = new Vector2(0.5f, 0.5f);
+		currentItemRect.anchoredPosition = new Vector2(0f, 0f);
+		currentItemRect.localRotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+	}
+
+
+}
