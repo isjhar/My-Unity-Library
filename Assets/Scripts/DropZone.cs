7f4bdcac0b172e214667aa2b2c07fe9474e15dd6
Isjhar <isjhar@gmail.com>
2016-03-17 09:52:45 +0700

    nambah drag and drop

---
+++ Assets/Scripts/DropZone.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityEngine.EventSystems
+{
+	public abstract class DropZone : MonoBehaviour, IDropHandler 
+	{
+		#region IDropHandler implementation
+		public abstract void OnDrop (PointerEventData eventData);
+		#endregion
+	}
+}
