6d8296bdd4cced8d64a9a6958950fa0c280992e9
Isjhar <isjhar@gmail.com>
2016-03-17 13:01:50 +0700

    rename draganddrop sample dan nambah contoh penggunaan shufflemanager

---
+++ Assets/Ignore/Scripts/ShuffleSample.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShuffleSample : MonoBehaviour 
+{
+	[SerializeField] int[] _intList;
+
+	private void Start()
+	{
+		Debug.Log("Before Shuffle : " + ToString(_intList));	
+		Debug.Log("Normal Shuffle : " + ToString(ShuffleItemManager.Shuffle<int>(_intList)));
+		Debug.Log("Non Repeatable Shuffle" + ToString(ShuffleItemManager.NonRepeatableItemPositionShuffle<int>(_intList)));
+	}
+
+	private string ToString(int[] intList)
+	{
+		string output = intList[0].ToString();
+		for(int i = 1; i < intList.Length; i++)
+		{
+			output += ", " + intList[i];
+		}
+		return output;
+	}
+}
