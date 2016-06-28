de7de5b0840446b9620dec11172d84bb5f3955f3
Isjhar <isjhar@gmail.com>
2016-06-28 11:53:50 +0700

    nambah vertical item generator example

---
+++ Assets/Scripts/VerticalItemGeneratorExample.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VerticalItemGeneratorExample : MonoBehaviour 
+{
+	[SerializeField] private GameObject _itemPrefab = null;
+	[SerializeField] private RectTransform _listHolder = null;
+	[SerializeField] private List<string> _titleList = new List<string>();
+	[SerializeField] private ListViewVerticalGenerator _viewGenerator = null;
+
+	// Use this for initialization
+	void Start () 
+	{
+		GenerateItem();
+	}
+	
+	public void GenerateItem()
+	{
+		for(int i = 0; i < _titleList.Count; i++)
+		{
+			GameObject item = Instantiate(_itemPrefab) as GameObject;
+			ItemView view = item.GetComponent<ItemView>();
+			view.Title.text = _titleList[i];
+			_viewGenerator.GenerateList(i, item, i == _titleList.Count - 1);
+		}
+	}
+}
