8a2d875f26b642c7d5c85d8f0041006c7fa432a0
Isjhar Kautsar <isjhar@gmail.com>
2016-10-23 15:34:39 +0700

    add tile canvas

---
+++ Assets/TileCanvas/Scripts/Editor/TileCanvasEditor.cs
@@ -0,0 +1,251 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace Isjhar.TileCanvas
+{
+	[CustomEditor(typeof(TileCanvas))]
+	public class TileCanvasEditor : Editor 
+	{
+		public enum MouseState
+		{
+			Down, Idle
+		}
+
+		public enum PaintState
+		{
+			Draw, Erase
+		}
+
+		public enum AltState
+		{
+			Idle, Hold
+		}
+
+		private TileCanvas _targetScript;
+		private TileCanvas.Coord _currentMousePositionCoord;
+		private MouseState _mouseState;
+		private AltState _altState;
+		private SerializedProperty _tempRowProp;
+		private SerializedProperty _tempColumnProp;
+		private SerializedProperty _rowProp;
+		private SerializedProperty _columnProp;
+		private SerializedProperty _selectedSpriteProp;
+		private bool isDirty = false;
+		private Sprite _drawSprite;
+
+		[SerializeField] private PaintState _paintState;
+
+
+		private void OnEnable()
+		{
+			_targetScript = (TileCanvas) target;
+			_currentMousePositionCoord = new TileCanvas.Coord ();
+			_mouseState = MouseState.Idle;
+			_altState = AltState.Idle;
+			_paintState = PaintState.Draw;
+
+			_tempRowProp = serializedObject.FindProperty ("_tempRow");
+			_tempColumnProp = serializedObject.FindProperty ("_tempColumn");
+			_selectedSpriteProp = serializedObject.FindProperty ("_selectedSprite");
+			_rowProp = serializedObject.FindProperty ("_row");
+			_columnProp = serializedObject.FindProperty ("_column");
+
+			if (_paintState == PaintState.Draw)
+			{
+				_drawSprite = _targetScript.SelectedSprite;
+			}
+			else if (_paintState == PaintState.Erase)
+			{
+				_drawSprite = null;
+			}
+			_targetScript.CheckTilesDefined ();
+		}
+
+		public override void OnInspectorGUI ()
+		{
+			serializedObject.Update ();
+			EditorGUI.BeginChangeCheck ();
+			EditorGUILayout.PropertyField (_selectedSpriteProp);
+			if (EditorGUI.EndChangeCheck ())
+			{
+				_drawSprite = (Sprite)_selectedSpriteProp.objectReferenceValue;
+			}
+			EditorGUI.BeginChangeCheck ();
+			EditorGUILayout.PropertyField (_tempRowProp, new GUIContent ("Row"));
+			EditorGUILayout.PropertyField (_tempColumnProp, new GUIContent ("Column"));	
+			if (EditorGUI.EndChangeCheck ())
+			{
+				if (_tempRowProp.intValue != _targetScript.Row || _tempColumnProp.intValue != _targetScript.Column)
+					isDirty = true;
+				else
+					isDirty = false;
+			}
+
+			GUI.enabled = isDirty;
+			if (GUILayout.Button ("Apply Changed"))
+			{
+				_targetScript.RecreateTiles (_tempRowProp.intValue, _tempColumnProp.intValue);
+				SceneView.RepaintAll ();
+				isDirty = false;
+			}
+			GUI.enabled = true;
+			EditorGUILayout.LabelField ("Draw Mode", EditorStyles.boldLabel);
+			GUI.enabled = !(_paintState == PaintState.Draw);
+			if (GUILayout.Button ("Draw"))
+			{
+				_drawSprite = _targetScript.SelectedSprite;
+				_paintState = PaintState.Draw;
+			}
+
+			GUI.enabled = !(_paintState == PaintState.Erase);
+			if (GUILayout.Button ("Erase"))
+			{
+				_drawSprite = null;
+				_paintState = PaintState.Erase;
+			}
+
+			serializedObject.ApplyModifiedProperties ();
+		}
+
+		private void OnSceneGUI()
+		{
+			Event current = Event.current;
+			int controlId = GUIUtility.GetControlID (FocusType.Passive);
+			Vector2 mousePosition = HandleUtility.GUIPointToWorldRay (Event.current.mousePosition).origin;
+			TileCanvas.Coord selectedCoord = GetCoordByMousePosition (mousePosition);
+
+			// get input alt
+			if (current.isKey)
+			{
+				if (current.type == EventType.KeyDown && current.keyCode == KeyCode.LeftAlt && _altState == AltState.Idle)
+				{
+					_altState = AltState.Hold;
+				}	
+				else if (current.type == EventType.keyUp && current.keyCode == KeyCode.LeftAlt && _altState == AltState.Hold)
+				{
+					_altState = AltState.Idle;
+				}	
+			}
+			if (_altState == AltState.Hold)
+				return;
+			
+			switch (current.type)
+			{
+				case EventType.MouseMove:
+					break;
+				case EventType.MouseDrag:
+					if (_mouseState == MouseState.Down)
+					{
+						if (IsCoordOutOfBound (selectedCoord))
+						{
+							// Fail Paint, Out Of Bound
+							// Debug.LogErrorFormat ("Fail Paint Tile At Coord {0}, Out of Bound", selectedCoord);	
+							return;
+						}
+
+						DrawTile (selectedCoord, _drawSprite);
+						Event.current.Use ();
+					}
+					break;
+				case EventType.MouseDown:
+					if (Event.current.isMouse && Event.current.type == EventType.MouseDown)
+					{
+						_mouseState = MouseState.Down;
+
+						if (IsCoordOutOfBound (selectedCoord))
+						{
+							// Fail Paint Tile Out Of Bound
+							// Debug.LogErrorFormat ("Fail Paint Tile At Coord {0}, Out of Bound", selectedCoord);	
+							return;
+						}
+
+						DrawTile (selectedCoord, _drawSprite);
+						Event.current.Use ();
+					}
+					break;
+				case EventType.MouseUp:
+					if (_mouseState == MouseState.Down)
+					{
+						_mouseState = MouseState.Idle;
+					}
+					break;
+				case EventType.Layout:
+					HandleUtility.AddDefaultControl (controlId);
+					break;
+			}
+		}
+
+		private TileCanvas.Coord GetCoordByMousePosition(Vector2 mousePosition)
+		{
+			Vector2 direction = mousePosition - (Vector2) _targetScript.transform.position;
+			_currentMousePositionCoord.X = Mathf.CeilToInt (direction.x) - 1;
+			_currentMousePositionCoord.Y = Mathf.CeilToInt (direction.y) - 1;
+			return _currentMousePositionCoord;
+		}
+
+		private Vector3 GetPositionByCoord(TileCanvas.Coord coord)
+		{
+			return new Vector3 (coord.X + 0.5f, coord.Y + 0.5f, _targetScript.transform.position.z);
+		}
+
+		private bool IsCoordOutOfBound(TileCanvas.Coord coord)
+		{
+			return coord.X < 0 || coord.Y < 0 || coord.X >= _targetScript.Column || coord.Y >= _targetScript.Row;
+		}
+
+		private void DrawTile(TileCanvas.Coord coord, Sprite sprite)
+		{
+			GameObject tileAtSelectedCoord = _targetScript.GetTile (coord);
+			if (sprite != null)
+			{
+				if (tileAtSelectedCoord != null)
+				{
+					SpriteRenderer tileSpriteRenderer = tileAtSelectedCoord.GetComponent <SpriteRenderer> ();
+					if (tileSpriteRenderer.sprite == sprite)
+					{
+						// Fail Paint Sprite Same
+						// Debug.LogErrorFormat ("Fail Paint Tile At Coord {0}, Sprite Same", coord);	
+						return;
+					}
+					else
+					{
+						// Replace Sprite
+						// Debug.LogFormat ("Replace Sprite At {0}", coord);	
+						tileSpriteRenderer.sprite = sprite;
+					}
+				}
+				else
+				{
+					// Create A New Tile
+					// Debug.LogFormat ("Create A New Tile At {0}", coord);	
+					tileAtSelectedCoord = new GameObject ();
+					tileAtSelectedCoord.name = coord.ToString ();
+					tileAtSelectedCoord.transform.position = GetPositionByCoord (coord);
+					SpriteRenderer tileSpriteRenderer = tileAtSelectedCoord.AddComponent <SpriteRenderer> ();
+					tileSpriteRenderer.sprite = sprite;
+					tileAtSelectedCoord.transform.parent = _targetScript.transform;
+					_targetScript.AddTile (coord, tileAtSelectedCoord);
+				}
+			}
+			else
+			{
+				if (tileAtSelectedCoord != null)
+				{
+					// Delete Tile
+					// Debug.LogFormat ("Delete Tile At {0}", coord);
+					DestroyImmediate (tileAtSelectedCoord);
+					_targetScript.AddTile (coord, null);
+				}
+			}
+		}
+
+		[MenuItem("GameObject/2D Object/Tile Canvas")]
+		private static void CreateTileCanvasGameObject()
+		{
+			GameObject tileCanvasGameObject = new GameObject ("Tile Canvas");
+			tileCanvasGameObject.AddComponent <TileCanvas>();
+			Selection.activeGameObject = tileCanvasGameObject;
+		}
+	}
+}
---
+++ Assets/TileCanvas/Scripts/TileCanvas.cs
@@ -0,0 +1,192 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Isjhar.TileCanvas
+{
+	[ExecuteInEditMode]
+	public class TileCanvas : MonoBehaviour 
+	{
+		[SerializeField] private int _tempRow;
+		[SerializeField] private int _tempColumn;
+		[SerializeField] private Sprite _selectedSprite = null;
+		[SerializeField] private GameObject[,] _tiles = null;
+
+		[SerializeField] private int _row = 0;
+		[SerializeField] private int _column = 0;
+
+		public Sprite SelectedSprite { get { return _selectedSprite; } }
+
+		public int Row
+		{
+			get
+			{
+				return _row;
+			}
+		}
+
+		public int Column
+		{
+			get
+			{
+				return _column;
+			}
+		}
+
+		public GameObject[,] Tiles
+		{
+			get
+			{
+				return _tiles;
+			}
+			set
+			{
+				_tiles = value;
+			}
+		}
+
+		// Use this for initialization
+		void Start () {
+
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+		}
+
+		public void OnDrawGizmosSelected()
+		{
+			Gizmos.color = Color.cyan;
+			if(_column > 0)
+				for (int x = 0; x <= _column; x++)
+				{
+					Gizmos.DrawLine (transform.position + new Vector3(x, 0f, 0f), transform.position + new Vector3(x, _row, 0f));
+				}
+
+			if(_row > 0)
+				for (int y = 0; y <= _row; y++)
+				{
+					Gizmos.DrawLine (transform.position + new Vector3(0f, y, 0f), transform.position + new Vector3(_column, y, 0f));
+				}
+		}
+
+		private void CheckTilesInited()
+		{
+			if (_tiles == null)
+			{
+				// Debug.Log ("Tiles Null, Create A New Tiles");
+				// Create A New Tiles
+				_tiles = new GameObject[_column, _row];
+			}
+		}
+
+		public GameObject GetTile(Coord coord)
+		{
+			CheckTilesInited ();
+			return _tiles [coord.X, coord.Y];
+		}
+
+		public void AddTile(Coord coord, GameObject tile)
+		{
+			CheckTilesInited ();
+			_tiles [coord.X, coord.Y] = tile;
+		}
+
+		public void CheckTilesDefined()
+		{
+			if (_tiles == null)
+			{
+				_tiles = new GameObject[_row, _column];
+				List<GameObject> deletedTiles = new List<GameObject> ();
+				foreach (Transform child in transform)
+				{
+					Coord childCoord = Coord.Parse (child.name);
+					try
+					{
+						_tiles[childCoord.X, childCoord.Y] = child.gameObject;
+					}
+					catch
+					{
+						deletedTiles.Add (child.gameObject);
+					}
+				}
+				while (deletedTiles.Count > 0)
+				{
+					DestroyImmediate (deletedTiles[0]);
+					deletedTiles.RemoveAt (0);
+				}
+			}
+		}
+
+		public void RecreateTiles(int row, int column)
+		{
+			GameObject[,] oldTiles = _tiles;
+			_row = row;
+			_column = column;
+			_tiles = new GameObject[_column, _row];
+			// ada tiles versi lama
+			if (oldTiles == null)
+				return;
+			
+			int copyBoundX = Mathf.Min (oldTiles.GetLength (0), _tiles.GetLength (0));
+			int copyBoundY = Mathf.Min (oldTiles.GetLength (1), _tiles.GetLength (1));
+			// reassign versi lama
+			for (int x = 0; x < oldTiles.GetLength (0); x++)
+			{
+				for (int y = 0; y < oldTiles.GetLength (1); y++)
+				{
+					if (x < copyBoundX && y < copyBoundY)
+					{
+						_tiles [x, y] = oldTiles [x, y];
+					}
+					else
+					{
+						if (oldTiles [x, y] != null)
+						{
+							DestroyImmediate (oldTiles [x, y].gameObject);
+							oldTiles[x, y] = null;
+						}
+					}
+				}
+			}
+		}
+
+		public struct Coord
+		{
+			private int _x;
+			private int _y;
+
+			public int X { get { return _x; } set { _x = value; } }
+			public int Y { get { return _y; } set { _y = value; } }
+
+			public Coord(int x, int y)
+			{
+				_x = x;
+				_y = y;
+			}
+
+			public override string ToString ()
+			{
+				return string.Format ("[Coord: X={0}, Y={1}]", X, Y);
+			}
+
+			public static Coord Parse(string format)
+			{
+				string[] splitResult = format.Split (' ');
+				try
+				{
+					string xString = splitResult[1].Substring (0, splitResult[1].Length - 1);
+					string yString = splitResult[2].Substring (0, splitResult[2].Length - 1);
+					int xValue = int.Parse (xString.Split ('=')[1]);
+					int yValue = int.Parse (yString.Split ('=')[1]);
+					return new Coord(xValue, yValue);
+				}
+				catch(System.Exception)
+				{
+					return new Coord (-1, -1);
+				}
+			}
+		}
+	}
+}
